edc266b8b9b7093aa6e1f3e740983af7e3d0a1a4
yefazhi <wb-yefazhi@tongdun.me>
2020-04-15 20:11:06 +0800

    '创建springboot模块和引入fastdfs'

---
+++ dfs-core/src/main/java/com/qianlicode/document/dfscore/DfsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.qianlicode.document.dfscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfsCoreApplication.class, args);
+	}
+
+}
---
+++ dfs-core/src/test/java/com/qianlicode/document/dfscore/DfsCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qianlicode.document.dfscore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DfsCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
